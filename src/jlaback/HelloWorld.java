e601fb99c24c6b7d3d2eb2620510fd218e209289
Jakob Laback <jlaback@student.tgm.ac.at>
2018-10-12 12:16:21 +0200

    Updated Hello World

--- src/jlaback/HelloWorld.java
+++ src/jlaback/HelloWorld.java
@@ -3,5 +3,7 @@ package jlaback;
 public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World1");
+		System.out.println("Hello World2");
 	}
 }
